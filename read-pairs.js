db58c000a36bcebfbc5fee85442e6c77d9c21aad
george <geomarray@gmail.com>
2018-12-20 22:04:59 -0800

    PR for submit

--- read-pairs.js
+++ read-pairs.js
@@ -5,34 +5,34 @@ const util = require('util');
 
 const dataPromise = util.promisify(fs.readFile);
 
-function toHTML(arr) {
+// function toHTML(arr) {
 
-    const tags = ['<h3>','</h3>','<ul>','</ul>','<li>','</li>']
-    for(let i = 0; i< arr.length ; i++){
-        let marker = 0;
-        if(arr[i] === '/n'){
-            arr.splice(marker,0,tags[0]);
-            arr.splice(i,0,tags[1]);
-            i++;
-        }else if(arr[i !== ' ']){
-            marker = i;
-            arr.splice(i,0,tags[2]);
-            arr.splice(i,0,tags[3]);
-            for(let j = i; i < arr.length; j++) {
-                if(arr[j]=== '/n'){
-                    arr.splice(j,0,tags[3]);
-                    i = j;
-                    marker = j;
-                    break;
-                }else if(arr[j]=== '.'){
-                    arr.splice(j,0,tags[4]);
-                    arr.splice(j,0,tags[2]);
-                    marker = j;
-                }
-            }
-        }
-    }
-}
+//     const tags = ['<h3>','</h3>','<ul>','</ul>','<li>','</li>']
+//     for(let i = 0; i< arr.length ; i++){
+//         let marker = 0;
+//         if(arr[i] === '/n'){
+//             arr.splice(marker,0,tags[0]);
+//             arr.splice(i,0,tags[1]);
+//             i++;
+//         }else if(arr[i !== ' ']){
+//             marker = i;
+//             arr.splice(i,0,tags[2]);
+//             arr.splice(i,0,tags[3]);
+//             for(let j = i; i < arr.length; j++) {
+//                 if(arr[j]=== '/n'){
+//                     arr.splice(j,0,tags[3]);
+//                     i = j;
+//                     marker = j;
+//                     break;
+//                 }else if(arr[j]=== '.'){
+//                     arr.splice(j,0,tags[4]);
+//                     arr.splice(j,0,tags[2]);
+//                     marker = j;
+//                 }
+//             }
+//         }
+//     }
+// }
     
 dataPromise("./files/pair-programming.txt", (err, data) =>{
     if (err) throw err;
@@ -41,23 +41,26 @@ dataPromise("./files/pair-programming.txt", (err, data) =>{
 
 })
 .then(data => {
+    //   console.log(data);
+    //     let stringifyBuffer = data =>{
+    //         let str = '';
 
-    let stringifyBuffer = data =>{
-        let str = '';
+    //         for(let char of data) {
+    //             str += String.fromCharCode(char);
+    //         }
 
-        for(let char of data) {
-            str += String.fromCharCode(char);
-        }
+    //         return str;
+    //     }
+    console.log(data.split(''))
+    // let stringed = stringifyBuffer(data);
+    console.log(data);
 
-        return str;
-    }
 
-    let stringed = stringifyBuffer(data);
-    stringed = '<article>' + stringed + "</article>";
+    // stringed = '<article>' + stringed + "</article>";
 
-    console.log(stringed);
+    // console.log(stringed);
 
-    
+    return "this call is done";
 
 
 });
